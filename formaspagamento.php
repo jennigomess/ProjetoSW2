fa700984c1da7b5232ad5fec9da043e8b0055708
jennigomess <jennifergomesoliveira.15@gmail.com>
2025-02-11 10:47:57 -0300

    11/02

--- formaspagamento.php
+++ formaspagamento.php
@@ -11,31 +11,6 @@
     
 </head>
 <body>
-
-    <div class="container-fluid bg-primary text-white text-center p-5">
-        <h1>Projeto SW2</h1>
-        <p>
-            Elaborado por Jennifer Gomes
-        </p>
-    </div>
-    
-    <div class="container">
-        <div class="row">
-
-            <div class="col-sm-4">
-                <h1>Administrador</h1>
-            </div>
-
-            <div class="col-sm-4">
-                <h1>Categorias</h1>
-            </div>
-
-            <div class="col-sm-4">
-                <h1>Fornecedores</h1>
-            </div>
-
-        </div>
-    </div>
-
+    <h1>Projeto SW2</h1>
 </body>
 </html>
