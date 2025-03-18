bf2086351e981777561a9e1e17e9c645e6aa1da7
jennigomess <jennifergomesoliveira.15@gmail.com>
2025-03-18 10:50:04 -0300

    18/03/25
    jjj

--- administrador.php
+++ administrador.php
@@ -19,7 +19,7 @@
 
                 <h3 class="text-center p-3">Preencha os dados abaixo</h3>
 
-                <form action="#">
+                <form action="administrador_cadastro.php">
 
                     <p>
                         Digite o nome do administrador<br>
