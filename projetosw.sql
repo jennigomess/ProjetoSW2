b0d135adfb3501fa9eac0fe16351110474c01511
jennigomess <jennifergomesoliveira.15@gmail.com>
2025-04-08 07:58:27 -0300

    08/04
    jj

--- projetosw.sql
+++ projetosw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 07-Abr-2025 às 17:44
+-- Tempo de geração: 08-Abr-2025 às 10:57
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -78,7 +78,14 @@ CREATE TABLE IF NOT EXISTS `formapagamento` (
   `codigo` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(200) NOT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `formapagamento`
+--
+
+INSERT INTO `formapagamento` (`codigo`, `nome`) VALUES
+(1, 'Cartão de Crédito');
 
 -- --------------------------------------------------------
 
@@ -99,6 +106,26 @@ CREATE TABLE IF NOT EXISTS `fornecedor` (
 
 INSERT INTO `fornecedor` (`codigo`, `nome`) VALUES
 (1, 'Motoka Motos');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+CREATE TABLE IF NOT EXISTS `produto` (
+  `codigo` int NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(200) NOT NULL,
+  `precocusto` float(10,2) NOT NULL,
+  `precovenda` float(10,2) NOT NULL,
+  `codcategoria` int NOT NULL,
+  `codfornecedor` int NOT NULL,
+  `imagem1` varchar(200) NOT NULL,
+  `imagem2` varchar(200) NOT NULL,
+  `imagem3` varchar(200) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
